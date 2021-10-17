25c22500e934a9c2425e03b24e44cba66f716ce4
ArmandoMarioto <armando_marioto100@hotmail.com>
2021-10-17 13:30:59 -0300

    documentação

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFiles from "./swagger.json";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFiles));
 
 app.use(express.json());
 
